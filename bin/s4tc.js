ab7e2a3f8d365eb81e74b990ab537c06f2ee22ca
Takeshi Horiuchi <keshihoriuchi@gmail.com>
2017-08-28 22:53:49 +0900

    0.0.2

--- bin/s4tc.js
+++ bin/s4tc.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const program = require("commander");
 
 program
-  .version("0.0.1")
+  .version("0.0.2")
   .option("-c, --cert <path>", "(required) cert file path")
   .option("-k, --key <path>", "(required) key file path")
   .option(
